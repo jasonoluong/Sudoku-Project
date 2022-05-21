0eacb1e38446957462b19f7b5c2f6206078adade
Jason Luong <jasononluong@gmail.com>
2022-05-21 14:04:04 +0100

    hard code gameboard and method to print gameboard

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
+class Sudoku {
+    public:
+    //variables
+    //2by2 matrix
+    int sudokuboard[9][9] = {
+                            {0, 0, 6, 5, 0, 8, 4, 0, 0},
+                            {5, 2, 0, 0, 0, 1, 0, 0, 0},
+                            {0, 7, 8, 0, 0, 0, 0, 0, 1},
+                            {0, 0, 4, 0, 1, 0, 0, 8, 0},
+                            {9, 0, 0, 8, 2, 3, 0, 0, 5},
+                            {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                            {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                            {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                            {0, 0, 5, 2, 0, 6, 3, 0, 0}
+                            };
+   
+    
+
+
+    void showgame(){
+        for(int i=0; i<9;i++){
+            for(int j=0; j<9;j++){
+                cout<<sudokuboard[i][j]<< " ";
+            }   
+            cout << "\n";
+        }
+     
+    }
+    
+    
+};
+
+
 int main(){
-    int x=10;
-    int y=10;//test
-    int z=100;
+    Sudoku sudokuboard;
+    sudokuboard.showgame();
+    return 0;
 }
